fb5700591ba80ae8f3c03c47c4379a4a915e2f8d
Twan Goosen <twan@clarin.eu>
2024-07-08 15:38:08 +0300

    initialized project with spring boot starter

---
+++ src/main/java/eu/clarin/cmdi/component_registry_ui/ComponentRegistryUiApplication.java
@@ -0,0 +1,13 @@
+package eu.clarin.cmdi.component_registry_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentRegistryUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentRegistryUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/clarin/cmdi/component_registry_ui/ComponentRegistryUiApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.clarin.cmdi.component_registry_ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentRegistryUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
