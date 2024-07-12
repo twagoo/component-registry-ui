9de1546680a9cf191e253fee087bd2cb2e945510
Twan Goosen <twan@clarin.eu>
2024-07-12 16:04:28 +0300

    testing generated API client

--- src/test/java/eu/clarin/cmdi/componentregistry/ui/ComponentRegistryRestClientTest.java
+++ src/test/java/eu/clarin/cmdi/componentregistry/ui/ComponentRegistryRestClientTest.java
@@ -2,11 +2,10 @@ package eu.clarin.cmdi.componentregistry.ui;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.openapitools.client.ApiClient;
+import org.openapitools.client.model.ProfileDescription;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClient;
 
 /**
@@ -17,39 +16,30 @@ import org.springframework.web.client.RestClient;
 @Slf4j
 public class ComponentRegistryRestClientTest {
 
-    @Autowired
-    private Jaxb2RootElementHttpMessageConverter xmlConverter;
-    @Autowired
-    private MappingJackson2HttpMessageConverter jsonConverter;
-
     public final String serviceUrl = "http://localhost:8080/ds/ComponentRegistry/rest/";
 
     @Test
     public void testConnect() {
-        RestClient client = RestClient.builder()
-                .requestFactory(new HttpComponentsClientHttpRequestFactory())
-                //.messageConverters(converters -> converters.add(new MyCustomMessageConverter()))
-                .baseUrl(serviceUrl)
-                //.defaultUriVariables(Map.of("variable", "foo"))
-                //.defaultHeader("My-Header", "Foo")
-                //.requestInterceptor(myCustomInterceptor)
-                //.requestInitializer(myCustomInitializer)
-                .messageConverters(list -> {
-                    list.add(xmlConverter);
-                    list.add(jsonConverter);
-                })
+        
+        RestClient restClient = ApiClient.buildRestClientBuilder().baseUrl(serviceUrl)
                 .build();
-
-        RestClient.ResponseSpec response = client.get()
+//        RestClient restClient = RestClient.builder()
+//                .requestFactory(new HttpComponentsClientHttpRequestFactory())
+//                //.messageConverters(converters -> converters.add(new MyCustomMessageConverter()))
+//                .baseUrl(serviceUrl)
+//                //.defaultUriVariables(Map.of("variable", "foo"))
+//                //.defaultHeader("My-Header", "Foo")
+//                //.requestInterceptor(myCustomInterceptor)
+//                //.requestInitializer(myCustomInitializer)
+//                .build();
+//
+        RestClient.ResponseSpec response = restClient.get()
                 .uri("registry/profiles/clarin.eu:cr1:p_1361876010587")
                 .retrieve();
-        
 //        ResponseEntity<List<ProfileDescription>> profiles= response.toEntity(new ParameterizedTypeReference<List<ProfileDescription>>() {
 //        });
 //        log.info("Profiles: {}", profiles);
-
-//        ResponseEntity<ProfileDescription> profile = response.toEntity(ProfileDescription.class);
-
-//        log.info("Profile: {}", profile);
+        ResponseEntity<ProfileDescription> profile = response.toEntity(ProfileDescription.class);
+        log.info("Profile: {}", profile);
     }
 }
