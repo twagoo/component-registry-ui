71f72a6fb74f541dea7ef232bce086a0b949457b
Twan Goosen <twan@clarin.eu>
2024-09-19 17:01:24 +0300

    API configuration in UI app

---
+++ component-registry-ui/src/main/java/eu/clarin/cmdi/componentregistry/ui/configuration/ApiClientConfiguration.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2024 twagoo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.clarin.cmdi.componentregistry.ui.configuration;
+
+import eu.clarin.cmdi.componentregistry.openapi.client.ApiClient;
+import eu.clarin.cmdi.componentregistry.openapi.client.api.DefaultApi;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author CLARIN ERIC
+ */
+@Configuration
+@Slf4j
+public class ApiClientConfiguration {
+
+    @Autowired
+    ApiClientConfigurationProperties clientConfig;
+
+    @Bean
+    public DefaultApi api() {
+        final DefaultApi api = new DefaultApi();
+
+        log.info("Applying configuration to API client");
+        //TODO: apply configuration properties
+        api.getApiClient().setBasePath(clientConfig.getBasePath());
+        return api;
+    }
+
+    @Bean
+    public ApiClient apiClient() {
+        return api().getApiClient();
+    }
+
+}
