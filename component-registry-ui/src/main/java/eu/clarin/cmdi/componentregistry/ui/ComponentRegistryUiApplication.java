34f2b3ed864eea9eb25f7583456b7b25893fe3e7
Twan Goosen <twan@clarin.eu>
2024-09-19 15:52:36 +0300

    Generated UI project

---
+++ component-registry-ui/src/main/java/eu/clarin/cmdi/componentregistry/ui/ComponentRegistryUiApplication.java
@@ -0,0 +1,13 @@
+package eu.clarin.cmdi.componentregistry.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentRegistryUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentRegistryUiApplication.class, args);
+	}
+
+}
