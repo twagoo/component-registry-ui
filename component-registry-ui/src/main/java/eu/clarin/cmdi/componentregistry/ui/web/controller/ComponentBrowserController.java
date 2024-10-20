8d0c833ad3319f445d4e104d590f5906fc2bcfb5
Twan Goosen <twan@clarin.eu>
2024-10-20 18:19:39 +0200

    composite 'browser' page

--- component-registry-ui/src/main/java/eu/clarin/cmdi/componentregistry/ui/web/controller/ComponentBrowserController.java
+++ component-registry-ui/src/main/java/eu/clarin/cmdi/componentregistry/ui/web/controller/ComponentBrowserController.java
@@ -26,6 +26,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestClientResponseException;
 
 /**
  *
@@ -33,22 +34,45 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class ComponentBrowserController {
-    
+
     private final DefaultApi api;
-    
+
+    public static final String SORT_BY_QUERY_PARAM = "sortBy";
+    public static final String SORT_BY_DEFAULT = "name";
+    public static final String SORT_DIRECTION_QUERY_PARAM = "sortDirection";
+    public static final String SORT_DIRECTION_DEFAULT = "ASC";
+
     @Autowired
     public ComponentBrowserController(DefaultApi api) {
         this.api = api;
     }
-    
-    @GetMapping(path = "/")
-    public String items(Model model, @RequestParam(defaultValue = "name") String sortBy, @RequestParam(defaultValue = "ASC") String sortDirection) {
+
+    private void setModelAttributes(String sortBy, String sortDirection, Model model) throws RestClientResponseException {
         final List<BaseDescription> items = api.getItems(sortBy, sortDirection);
         model.addAttribute("items", items);
         model.addAttribute("sortedBy", sortBy);
         model.addAttribute("sortedDirection", sortDirection);
         model.addAttribute("fields", Arrays.asList("id", "name", "description"));
+    }
+
+    @GetMapping(path = "/")
+    public String browser(Model model,
+            @RequestParam(name = SORT_BY_QUERY_PARAM,
+                    defaultValue = SORT_BY_DEFAULT) String sortBy,
+            @RequestParam(name = SORT_DIRECTION_QUERY_PARAM,
+                    defaultValue = SORT_DIRECTION_DEFAULT) String sortDirection) {
+        setModelAttributes(sortBy, sortDirection, model);
+        return "browser/browser";
+    }
+
+    @GetMapping(path = "/items")
+    public String items(Model model,
+            @RequestParam(name = SORT_BY_QUERY_PARAM,
+                    defaultValue = SORT_BY_DEFAULT) String sortBy,
+            @RequestParam(name = SORT_DIRECTION_QUERY_PARAM,
+                    defaultValue = SORT_DIRECTION_DEFAULT) String sortDirection) {
+        setModelAttributes(sortBy, sortDirection, model);
         return "browser/items";
     }
-    
+
 }
