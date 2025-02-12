c358887ff5a33c1f9b51b603745130150ff405a1
Twan Goosen <twan@clarin.eu>
2025-02-12 16:58:37 +0200

    submit vocabulary items from table

--- component-registry-ui/src/main/java/eu/clarin/cmdi/componentregistry/ui/web/controller/EditorController.java
+++ component-registry-ui/src/main/java/eu/clarin/cmdi/componentregistry/ui/web/controller/EditorController.java
@@ -29,7 +29,7 @@ import eu.clarin.cmdi.componentregistry.openapi.client.model.VocabularyType;
 import eu.clarin.cmdi.componentregistry.ui.service.ComponentSpecTransformationException;
 import eu.clarin.cmdi.componentregistry.ui.service.ComponentSpecTransformationService;
 import static eu.clarin.cmdi.componentregistry.ui.service.TransformationActions.*;
-import java.util.Optional;
+import eu.clarin.cmdi.componentregistry.ui.web.controller.model.VocabularyDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -53,17 +53,17 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value = "/editor")
 @Slf4j
 public class EditorController {
-    
+
     private final ItemsControllerApi api;
-    
+
     private final ComponentSpecTransformationService specTransformationService;
-    
+
     @Autowired
     public EditorController(ItemsControllerApi api, ComponentSpecTransformationService specTransformationService) {
         this.api = api;
         this.specTransformationService = specTransformationService;
     }
-    
+
     @GetMapping(path = "/{itemId}")
     public String editor(@PathVariable String itemId, Model model) throws ErrorResponseException {
         final BaseDescription description = api.getItem(itemId);
@@ -71,14 +71,14 @@ public class EditorController {
             throw new ErrorResponseException(HttpStatus.NOT_FOUND);
         } else {
             final ComponentSpec spec = api.getItemSpec(itemId, MediaType.APPLICATION_JSON_VALUE);
-            
+
             model.addAttribute("description", description);
             model.addAttribute("spec", spec);
-            
+
             return "editor/editor";
         }
     }
-    
+
     @PostMapping(path = "/{itemId}/spec")
     public String submitSpec(@PathVariable String itemId, ComponentSpec formData, BindingResult bindingResult, Model model) {
         log.info("Item: {}, Incoming data: {}", itemId, formData);
@@ -86,17 +86,17 @@ public class EditorController {
         // https://stackoverflow.com/questions/30280131/thymeleaf-spring-nested-backing-object-is-not-binding-the-values-on-form-submit
         return editor(itemId, model);
     }
-    
+
     @PostMapping(path = "/transform")
     public String performOperation(ComponentSpec spec,
             @RequestParam String operation,
             @RequestParam String path,
             Model model) throws JsonProcessingException, ComponentSpecTransformationException {
         final ComponentSpec transformedSpec = transform(operation, spec, path);
-        
+
         model.addAttribute("componentId", spec.getHeader().getId());
         model.addAttribute("spec", transformedSpec);
-        
+
         return "editor/fragments/specForm :: specForm";
     }
 
@@ -115,26 +115,26 @@ public class EditorController {
             return new ModelAndView("redirect:/editor/{itemId}", ImmutableMap.of("itemId", itemId));
         }
     }
-    
+
     @GetMapping("/referencedComponent/{componentId}")
     public String getReferencedComponent(@PathVariable String componentId, Model model) {
         final ComponentSpec spec = api.getItemSpec(componentId, null);
         model.addAttribute("spec", spec);
         return "/editor/fragments/componentRef :: expandedComponent";
     }
-    
+
     @PostMapping("/elementValueSchemeEditor")
     public String valueSchemeEditor(ComponentSpec spec, @RequestParam String path, Model model) {
         try {
             final ElementType element = specTransformationService.extractElement(spec, path);
             final ValueSchemeType valueScheme = element.getValueScheme();
             final ElementType.ValueSchemeAttributeEnum valueSchemeAttribute = element.getValueSchemeAttribute();
-            
+
             model.addAttribute("parentType", "element");
             model.addAttribute("parentPath", path);
             model.addAttribute("valueSchemeAttribute", valueSchemeAttribute);
             model.addAttribute("valueScheme", valueScheme);
-            
+
             if (valueScheme != null && valueScheme.getVocabulary() != null) {
                 model.addAttribute("selectedTab", "vocabulary");
             } else if (valueScheme != null && valueScheme.getPattern() != null) {
@@ -142,61 +142,58 @@ public class EditorController {
             } else {
                 model.addAttribute("selectedTab", "simple");
             }
-            
+
             return "/editor/fragments/valueScheme :: valueSchemeEditor";
         } catch (ComponentSpecTransformationException | JsonProcessingException ex) {
             log.error("Error while extracting element from spec at path " + path);
             throw new RuntimeException(ex);
         }
     }
-    
+
     @PostMapping("/elementValueSchemeEditor/simple")
     public String simpleValueScheme(@RequestParam String path, @RequestParam Attribute.ValueSchemeEnum type, Model model) {
         return valueScheme(model, path, type, null);
     }
-    
+
     @PostMapping("/elementValueSchemeEditor/pattern")
     public String patternValueScheme(@RequestParam String path, @RequestParam String pattern, Model model) {
         final ValueSchemeType valueScheme = new ValueSchemeType();
         valueScheme.setPattern(pattern);
-        
+
         return valueScheme(model, path, null, valueScheme);
     }
-    
+
     @PostMapping("/elementValueSchemeEditor/vocabulary")
-    public String vocabularyValueScheme(@RequestParam String path,
-            @RequestParam String vocabularyType,
-            @RequestParam Optional<String> externalVocabUri,
-            @RequestParam Optional<String> externalVocabValueProperty,
-            @RequestParam Optional<String> externalVocabValueLanguage,
-            Model model) {
+    public String vocabularyValueScheme(@RequestParam String path, VocabularyDTO vocabData, Model model) {
         final ValueSchemeType valueScheme = new ValueSchemeType();
-        final VocabularyType vocabulary = new VocabularyType();
-        valueScheme.setVocabulary(vocabulary);
-        
-        if(vocabularyType.equals("closed")) {
-            //TODO: process vocab definition
+
+        if (vocabData.getVocabularyType().equals("closed")) {
+            //TODO: validate vocab definition
+            //TODO: validate values of other properties
             log.debug("Closed vocabulary");
+        } else {
+            //TODO: validate URI (mandatory), 
+            //TODO: validate values of other properties
+            
+            //open vocabulary: the enumeration must not be set!
+            vocabData.getVocabulary().setEnumeration(null);
         }
-        
-        //set optional closed vocab properties
-        externalVocabUri.ifPresent(vocabulary::setUri);
-        externalVocabValueProperty.ifPresent(vocabulary::setValueProperty);
-        externalVocabValueLanguage.ifPresent(vocabulary::setValueLanguage);
-        
+
+        valueScheme.setVocabulary(vocabData.getVocabulary());
+
         return valueScheme(model, path, null, valueScheme);
     }
-    
+
     private String valueScheme(Model model, String path, final Attribute.ValueSchemeEnum valueSchemeAttribute, final ValueSchemeType valueScheme) {
         model.addAttribute("parentPath", path);
         model.addAttribute("valueSchemeAttributePath", path + ".valueSchemeAttribute");
         model.addAttribute("valueSchemeAttributeValue", valueSchemeAttribute);
         model.addAttribute("valueSchemePath", path + ".valueScheme");
         model.addAttribute("valueSchemeValue", valueScheme);
-        
+
         return "/editor/fragments/valueScheme :: valueScheme";
     }
-    
+
     private ComponentSpec transform(String operation, ComponentSpec spec, String path) throws ComponentSpecTransformationException, JsonProcessingException {
         return switch (operation) {
             case NOOP ->
@@ -233,12 +230,12 @@ public class EditorController {
             }
         };
     }
-    
+
     @GetMapping("/newDocumentationElement")
     public String newDocumentationElement(@RequestParam String path, Model model) {
         model.addAttribute("path", path);
         model.addAttribute("doc", new DocumentationType());
         return "/editor/fragments/documentation :: documentationElement";
     }
-    
+
 }
---
+++ component-registry-ui/src/main/java/eu/clarin/cmdi/componentregistry/ui/web/controller/model/VocabularyDTO.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2025 CLARIN ERIC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.clarin.cmdi.componentregistry.ui.web.controller.model;
+
+import eu.clarin.cmdi.componentregistry.openapi.client.model.VocabularyType;
+import lombok.Data;
+
+/**
+ *
+ * @author twagoo
+ */
+@Data
+public class VocabularyDTO {
+
+    String vocabularyType;
+    VocabularyType vocabulary;
+
+}
